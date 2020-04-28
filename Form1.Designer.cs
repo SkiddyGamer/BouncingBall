d11039f2fcbb854fa351df9dfd0adb8f27d789b6
Skiddy Gamer <karels.jonass@gmail.com>
2020-04-28 16:31:09 +0300

    version 16:30

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BouncingBall
 {
-    partial class Form1
+    partial class Field
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Coral;
+            this.Ball.Location = new System.Drawing.Point(304, 51);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(100, 100);
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Ball);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Ball;
     }
 }
 
